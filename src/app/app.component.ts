2056712a7f9448f82be3581bae10b9297c488fbb
Jancy <s.jancyjerin@gmail.com>
2023-07-05 23:45:30 -0500

    file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-frontendproject';
+  title = 'Angular 10 Spring boot CRUD Full Stack App';
 }
